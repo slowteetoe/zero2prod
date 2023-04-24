f531ca0a9f63b0bfd621f1e7418850b6f15e4520
Steven Lotito <steven.lotito@gmail.com>
2023-04-24 07:56:49 -0700

    add telemetry logging

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,6 +1,7 @@
 use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
+use tracing::{Instrument};
 use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
@@ -10,6 +11,17 @@ pub struct FormData {
 }
 
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    let request_id = Uuid::new_v4().to_string();
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber",
+        %request_id,
+        subscriber_email = %form.email,
+        subscriber_name = %form.name
+    );
+    let _request_span_guard = request_span.enter();
+
+    let query_span = tracing::info_span!("Saving new subscriber details in the database");
+
     match sqlx::query!(
         r#"
     INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -21,24 +33,33 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
         Utc::now()
     )
     .execute(pool.get_ref())
+    .instrument(query_span)
     .await
     {
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(_) => {
+            tracing::info!("Details saved");
+            HttpResponse::Ok().finish()
+        }
         Err(e) => match e {
             sqlx::Error::Database(err) => {
                 if err.constraint() == Some("subscriptions_email_key") {
-                    println!("User is already subscribed");
+                    tracing::info!("request_id {} User is already subscribed", request_id);
                     HttpResponse::NoContent().finish()
                 } else {
-                    println!(
-                        "Failed to insert subscription due to database error: {}",
+                    tracing::error!(
+                        "request_id {} Failed to insert subscription due to database error: {:?}",
+                        request_id,
                         err
                     );
                     HttpResponse::InternalServerError().finish()
                 }
             }
             _ => {
-                println!("Failed to insert subscription: {}", e);
+                tracing::error!(
+                    "request_id {} Failed to insert subscription: {:?}",
+                    request_id,
+                    e
+                );
                 HttpResponse::InternalServerError().finish()
             }
         },
--- src/startup.rs
+++ src/startup.rs
@@ -2,6 +2,7 @@ use std::net::TcpListener;
 
 use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
+use actix_web::middleware::Logger;
 use actix_web::{web, App, HttpServer};
 use sqlx::PgPool;
 
@@ -10,6 +11,7 @@ pub fn run(listener: TcpListener, connection_pool: PgPool) -> Result<Server, std
     let connection_pool = web::Data::new(connection_pool);
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .route("/health", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(connection_pool.clone())
