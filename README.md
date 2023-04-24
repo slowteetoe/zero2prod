9f4db886df08533ef166ed46496a0f6ad2073256
Steven Lotito <steven.lotito@gmail.com>
2023-04-24 09:36:50 -0700

    allow showing test logs via TEST_LOG=true env var

--- README.md
+++ README.md
@@ -3,3 +3,17 @@
 ![Build pipeline](https://github.com/slowteetoe/zero2prod/actions/workflows/general.yml/badge.svg)
 
 Working along the book examples from [Zero to Production in Rust by Luca Palmieri](https://www.zero2prod.com/)
+
+## Testing
+
+We are using the `bunyan` CLI to prettify output logs. The original Bunyan requires NPM, but you can install a Rust port with:
+
+```sh
+cargo install bunyan
+```
+
+To see the logs:
+
+```sh
+TEST_LOG=true cargo test | bunyan
+```
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -10,8 +10,17 @@ use zero2prod::{
 
 // Ensure that the `tracing` stack is only initialized once
 static TRACING: Lazy<()> = Lazy::new(|| {
-    let subscriber = get_subscriber("test".into(), "debug".into());
-    init_subscriber(subscriber);
+    let default_filter_level = "info".to_string();
+    let subscriber_name = "test".to_string();
+    // We cannot assign the output of `get_subscriber` to a variable based on the value TEST_LOG because the sink is part of the type
+    // returned by `get_subscriber` so they are not the same type.  We could work around it, but this is the most straight-forward way of moving forward
+    if std::env::var("TEST_LOG").is_ok() {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::stdout);
+        init_subscriber(subscriber);
+    } else {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::sink);
+        init_subscriber(subscriber);
+    }
 });
 
 #[derive(Debug)]
