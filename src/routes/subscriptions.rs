e486a64b52b6f90cb195dd6d03abb21559e6dd61
Steven Lotito <steven.lotito@gmail.com>
2023-04-24 18:07:09 -0700

    clean up some tracing

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,7 +1,6 @@
 use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
-use tracing::Instrument;
 use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
@@ -10,19 +9,27 @@ pub struct FormData {
     name: String,
 }
 
+#[tracing::instrument(name="Adding a new subscriber", skip(form, pool), fields(request_id=%Uuid::new_v4(), subscriber_email=%form.email, subscriber_name=%form.name))]
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
-    let request_id = Uuid::new_v4().to_string();
-    let request_span = tracing::info_span!(
-        "Adding a new subscriber",
-        %request_id,
-        subscriber_email = %form.email,
-        subscriber_name = %form.name
-    );
-    let _request_span_guard = request_span.enter();
+    match insert_subscriber(&pool, &form).await {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(e) => match e {
+            sqlx::Error::Database(err) => {
+                if err.constraint() == Some("subscriptions_email_key") {
+                    tracing::info!("User is already subscribed");
+                    HttpResponse::NoContent().finish()
+                } else {
+                    HttpResponse::InternalServerError().finish()
+                }
+            }
+            _ => HttpResponse::InternalServerError().finish(),
+        },
+    }
+}
 
-    let query_span = tracing::info_span!("Saving new subscriber details in the database");
-
-    match sqlx::query!(
+#[tracing::instrument(name = "Saving new subscriber details in db", skip(form, pool))]
+pub async fn insert_subscriber(pool: &PgPool, form: &FormData) -> Result<(), sqlx::Error> {
+    sqlx::query!(
         r#"
     INSERT INTO subscriptions (id, email, name, subscribed_at)
     VALUES ($1, $2, $3, $4)
@@ -32,36 +39,11 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
         form.name,
         Utc::now()
     )
-    .execute(pool.get_ref())
-    .instrument(query_span)
+    .execute(pool)
     .await
-    {
-        Ok(_) => {
-            tracing::info!("Details saved");
-            HttpResponse::Ok().finish()
-        }
-        Err(e) => match e {
-            sqlx::Error::Database(err) => {
-                if err.constraint() == Some("subscriptions_email_key") {
-                    tracing::info!("request_id {} User is already subscribed", request_id);
-                    HttpResponse::NoContent().finish()
-                } else {
-                    tracing::error!(
-                        "request_id {} Failed to insert subscription due to database error: {:?}",
-                        request_id,
-                        err
-                    );
-                    HttpResponse::InternalServerError().finish()
-                }
-            }
-            _ => {
-                tracing::error!(
-                    "request_id {} Failed to insert subscription: {:?}",
-                    request_id,
-                    e
-                );
-                HttpResponse::InternalServerError().finish()
-            }
-        },
-    }
+    .map_err(|e| {
+        tracing::error!("Failed to execute query: {:?}", e);
+        e
+    })?;
+    Ok(())
 }
