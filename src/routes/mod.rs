7db6dab9c4334a122b9b1a1d3f738122538174ca
Steven Lotito <steven.lotito@gmail.com>
2023-05-06 15:54:15 -0700

    simple login form, no logic

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,9 +1,13 @@
 pub(crate) mod health_check;
+pub mod home;
+mod login;
 pub(crate) mod newsletter;
 pub(crate) mod subscriptions;
 pub(crate) mod subscriptions_confirm;
 
 pub use health_check::*;
+pub use home::*;
+pub use login::*;
 pub use newsletter::*;
 pub use subscriptions::*;
 pub use subscriptions_confirm::*;
--- src/startup.rs
+++ src/startup.rs
@@ -3,7 +3,9 @@ use std::net::TcpListener;
 use crate::configuration::{DatabaseSettings, Settings};
 use crate::domain::SubscriberEmail;
 use crate::email_client::EmailClient;
-use crate::routes::{confirm, health_check, publish_newsletter, subscribe};
+use crate::routes::{
+    confirm, health_check, home, login, login_form, publish_newsletter, subscribe,
+};
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
 use sqlx::postgres::PgPoolOptions;
@@ -71,6 +73,9 @@ pub fn run(
             .route("/subscriptions", web::post().to(subscribe))
             .route("/subscriptions/confirm", web::get().to(confirm))
             .route("/newsletters", web::post().to(publish_newsletter))
+            .route("/login", web::get().to(login_form))
+            .route("/", web::get().to(home))
+            .route("/login", web::post().to(login))
             .app_data(connection_pool.clone())
             .app_data(email_client.clone())
             .app_data(base_url.clone())
