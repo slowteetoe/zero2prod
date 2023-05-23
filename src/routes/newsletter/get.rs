5e131c7752d86b7cfeeb9c85b89df68f8f2a2db1
Steven Lotito <steven.lotito@gmail.com>
2023-05-22 19:47:29 -0700

    lol, one nice looking page. need to find a templating engine

--- src/routes/newsletter/get.rs
+++ src/routes/newsletter/get.rs
@@ -11,40 +11,68 @@ pub async fn publish_newsletter_form(flash_messages: IncomingFlashMessages) -> H
         .content_type(ContentType::html())
         .body(format!(
             r#"<!DOCTYPE html>
-        <html lang="en">
-          <head>
-            <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-            <title>Change Password</title>
-          </head>
-          <body>
-            {msg_html}
-            <form method="post" action="/admin/newsletters">
-              <label
-                >Title
-                <input
-                  type="text"
-                  placeholder="Choose a title for the newsletter"
-                  name="title"
-                />
-              </label>
-              <label for="content.html"
-                >HTML content
-                <textarea rows="4" cols="60" required="true" name="content.html"
-                  placeholder="Enter newsletter HTML content"
-                />
-              </label>
-              <label for="content.text
-                >Plaintext content
-                <textarea rows="4" cols="60" required="true" name="content.text"
-                  placeholder="Enter newsletter plain text content"
-                />
-              </label>
-              <br />
-              <button type="submit">Send Newsletter</button>
-            </form>
-            <p><a href="/admin/dashboard">&lt;- Back</a></p>
-          </body>
-        </html>        
+            <html lang="en">
+              <head>
+                <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+                <title>Change Password</title>
+                <style>
+                  *, *:before, *:after {{
+                    box-sizing: border-box;
+                  }}
+                  label {{
+                    padding: 10px;
+                    display: block;
+                  }}
+                  input[type=text], textarea {{
+                    padding: 10px;
+                    width: 60%;
+                    margin: 10px 0;
+                    border: 0;
+                    box-shadow:0 0 15px 4px rgba(0,0,0,0.26);
+                    border-radius:10px;
+                  }}
+                  button {{
+                      /* remove default behavior */
+                      appearance:none;
+                      -webkit-appearance:none;
+    
+                      /* usual styles */
+                      padding:10px;
+                      border:none;
+                      background-color:#3F51B5;
+                      color:#fff;
+                      font-weight:600;
+                      border-radius:5px;
+                      width:60%;
+                  }}
+                </style>
+              </head>
+              <body>
+                {msg_html}
+                <form method="post" action="/admin/newsletters">
+                  <div>
+                    <label for="title">Title</label>
+                      <input
+                        type="text"
+                        placeholder="Choose a title for the newsletter"
+                        name="title" required="true"/>
+                  </div>
+                  <br>
+                  <div>
+                    <label for="content.html">HTML content</label>
+                    <textarea rows="4" cols="60" required="true" name="content.html"
+                      placeholder="Enter newsletter HTML content"></textarea>
+                  </div>
+                  <br>
+                  <label for="content.text">Plaintext content</label>
+                  <textarea rows="4" cols="60" required="true" name="content.text"
+                    placeholder="Enter newsletter plain text content"></textarea>
+                  <br>
+                  <button type="submit">Send Newsletter</button>
+                </form>
+                <p><a href="/admin/dashboard">&lt;- Back</a></p>
+              </body>
+            </html>
         "#,
         ))
 }
